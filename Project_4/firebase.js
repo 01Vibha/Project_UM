0acdb8d9c5d75bc959742e97dae061c12890adef
Vibha Barse <140709853+01Vibha@users.noreply.github.com>
2024-11-20 20:41:58 +0530

    Add files via upload

---
+++ Project_4/firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAmLovND1nYUyGPv7P9953iF3fX0I_dMjw",
+    authDomain: "gas-booking-43470.firebaseapp.com",
+    projectId: "gas-booking-43470",
+    storageBucket: "gas-booking-43470.firebasestorage.app",
+    messagingSenderId: "462125863125",
+    appId: "1:462125863125:web:6c9a8d65c64f2d6d8f125c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
